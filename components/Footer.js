fb10b7da7c78077836cac04fa68c3696acdc5af2
unknown <yamim.hassan@northsout.edu>
2021-09-25 21:24:26 +0600

    about page added

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {FaFacebook,FaLinkedin,FaYoutube,FaGithub,FaTwitter} from 'react-icons/fa'
+import {FaFacebook,FaLinkedin,FaYoutube,FaGithub,FaTwitter,FaJava} from 'react-icons/fa'
 
 export default function Footer() {
     return (
-        <div className='w-full bg-blue-500'>
+        <div className='w-full bg-gray-700'>
             <div className='w-full flex justify-center items-center h-20 text-white sm:w-8/12 mx-auto'>
         
                 <span><i className='px-10 text-xl border-red-600'><FaFacebook/></i></span>
---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/Link'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+export default function about() {
+    return (
+        <div>
+            <Navbar/>
+            <h1>about</h1>
+            <Footer/>
+        </div>
+    )
+}
